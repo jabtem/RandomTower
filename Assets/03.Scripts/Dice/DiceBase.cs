e0013dc55a12e5f463463d1780b0aea55475b0cc
jabtem <suchie777@gmail.com>
2022-01-23 01:58:21 +0900

    3차 작업물저장

--- Assets/03.Scripts/Dice/DiceBase.cs
+++ Assets/03.Scripts/Dice/DiceBase.cs
@@ -9,8 +9,7 @@ public class DiceBase : MonoBehaviour
         fire,electric,wind,
     }
 
-    //주사위 등급
-    int grade;
+
 
     public SpriteRenderer[] dice_Eyes;
     SpriteRenderer spr;
@@ -18,9 +17,27 @@ public class DiceBase : MonoBehaviour
 
     MonsterSpawner monsterSpawner;
 
+    //드래그가능여부, 적군다이스는 드래그할수없어야되기때문
     bool dragOk;
+
+    //공격가능여부, 드래그중에는 공격이 발사되면안되기때문
+    bool attackOk;
+    [SerializeField]
+    bool gradeUp;
+    //등급업시킬 대상다이스
+    [SerializeField]
+    DiceBase gradeUpTarget;
+
+
+    bool isDrag;
+    Vector2 startPos;
+
     public bool DragOk
     {
+        get
+        {
+            return dragOk;
+        }
         set
         {
             dragOk = value;
@@ -34,16 +51,28 @@ public class DiceBase : MonoBehaviour
             monsterSpawner = value;
         }
     }
-
-    float startPosX;
-    float startPosY;
-
-    private void Awake()
+    DiceType type;
+    public DiceType Type
     {
-        spr = GetComponent<SpriteRenderer>();
+        get
+        {
+            return type;
+        }
+        set
+        {
+            type = value;
+            SetDiceColor();
+        }
     }
+    //주사위 등급
+    int grade;
     public int Grade
     {
+        get
+        {
+            return grade;
+        }
+
         set
         {
             grade = value;
@@ -60,44 +89,113 @@ public class DiceBase : MonoBehaviour
         }
     }
 
-    DiceType type;
-    public DiceType Type
+
+    private void Awake()
     {
-        get
-        {
-            return type;
-        }
-        set
-        {
-            type = value;
-            SetDiceColor();
-        }
+        spr = GetComponent<SpriteRenderer>();
     }
+    private void OnEnable()
+    {
+        startPos = transform.position;
+    }
+    private void Update()
+    {
+        if((Vector2)transform.position != startPos && !isDrag) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, startPos, 0.1f);
+        }
+
+    }
+
     private void OnMouseDown()
     {
-#if UNITY_EDITOR
-        if(Input.GetMouseButton(0))
-        {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            startPosX = mousePos.x - transform.position.x;
-            startPosY = mousePos.y - transform.position.y;
-            Debug.Log("test");
-        }
-
-
-#endif
-#if UNITY_ANDROID
-        if (Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-
-        }
-#endif
+        SortingOrderSet(12);
     }
 
     private void OnMouseDrag()
     {
-        
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
+        {
+            if (!isDrag)
+                isDrag = true;
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(mousePos.x, mousePos.y);
+        }
+
+
+#endif
+#if (UNITY_ANDROID || UNITY_IPHONE) 
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 tochPos = Camera.main.ScreenToWorldPoint(touch.position);
+                transform.position = new Vector2(tochPos.x, tochPos.y);
+            }
+        }
+
+#endif
     }
+
+    private void OnMouseUp()
+    {
+        if(!gradeUp)
+        {
+            isDrag = false;
+        }
+        else if(gradeUp)
+        {
+            int ranType = Random.Range(0, 3);
+
+            gradeUpTarget.Type = (DiceType)ranType;
+            gradeUpTarget.Grade += 1;
+            transform.position = startPos;
+            gameObject.SetActive(false);
+        }
+
+        SortingOrderSet(10);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.tag == "Dice")
+        {
+
+            DiceBase collisionDice = collision.gameObject.GetComponent<DiceBase>();
+
+            //타워의 타입이 같고 등급이 같으면 등급업가능
+            if (collisionDice.Type == type && collisionDice.Grade == grade && isDrag &&collisionDice.DragOk)
+            {
+                gradeUpTarget = collisionDice;
+                gradeUp = true;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Dice")
+        {
+            gradeUpTarget = null;
+            gradeUp = false;
+        }
+    }
+
+    void SortingOrderSet(int layer)
+    {
+        spr.sortingOrder = layer;
+        foreach (var eye in dice_Eyes)
+        {
+            eye.sortingOrder = layer+1;
+        }
+    }
+
+
     void SetDiceEyePositon(int grade)
     {
         switch (grade)
